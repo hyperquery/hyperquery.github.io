759c27a8bfa0bd41475a5b5fac8625e59e8c0ec4
paraterrestrial <paraterrestrial@riseup.net>
2020-04-19 02:05:00 -0400

    Updates

---
+++ precache-manifest.b926e1ab95a9cbf9653709f7c3601933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519bfe1784cba03d70f55bf9b604aaa1",
+    "url": "https://hyperquery.github.io/index.html"
+  },
+  {
+    "revision": "418c918af87c21950c5e",
+    "url": "https://hyperquery.github.io/static/css/main.5d2f9616.chunk.css"
+  },
+  {
+    "revision": "0ee5d49674ec97d87276",
+    "url": "https://hyperquery.github.io/static/js/2.e8d2f422.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://hyperquery.github.io/static/js/2.e8d2f422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418c918af87c21950c5e",
+    "url": "https://hyperquery.github.io/static/js/main.2937c393.chunk.js"
+  },
+  {
+    "revision": "75610eb6c3a0c358c429",
+    "url": "https://hyperquery.github.io/static/js/runtime-main.fbf14da4.js"
+  }
+]);
