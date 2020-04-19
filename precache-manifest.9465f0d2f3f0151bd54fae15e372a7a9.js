d7258a23e0618f6993d9d1ea0880c572ed14b356
paraterrestrial <paraterrestrial@riseup.net>
2020-04-19 04:21:03 -0400

    Updates

--- precache-manifest.9465f0d2f3f0151bd54fae15e372a7a9.js
+++ precache-manifest.9465f0d2f3f0151bd54fae15e372a7a9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ed613f00fc2fedab3527fdf3f18811b",
+    "revision": "3ec42f0cbaebc5e65b6a15a1fcdb6ed4",
     "url": "https://hyperquery.github.io/index.html"
   },
   {
-    "revision": "7507aa53471c0f48c538",
+    "revision": "421c3833adf20c709ba4",
     "url": "https://hyperquery.github.io/static/css/main.5d2f9616.chunk.css"
   },
   {
-    "revision": "6b8d010c75be67b3416d",
-    "url": "https://hyperquery.github.io/static/js/2.eaab31c5.chunk.js"
+    "revision": "aa8588e58641f9263abf",
+    "url": "https://hyperquery.github.io/static/js/2.1cca3c1d.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "https://hyperquery.github.io/static/js/2.eaab31c5.chunk.js.LICENSE.txt"
+    "url": "https://hyperquery.github.io/static/js/2.1cca3c1d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7507aa53471c0f48c538",
-    "url": "https://hyperquery.github.io/static/js/main.360e5810.chunk.js"
+    "revision": "421c3833adf20c709ba4",
+    "url": "https://hyperquery.github.io/static/js/main.207c5645.chunk.js"
   },
   {
     "revision": "75610eb6c3a0c358c429",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://hyperquery.github.io/precache-manifest.315de6c43f0698a253c8fa0ccf069c8d.js"
+  "https://hyperquery.github.io/precache-manifest.9465f0d2f3f0151bd54fae15e372a7a9.js"
 );
 
 self.addEventListener('message', (event) => {
